96f1c9fb0ffd8f9bd11a1da1c8f1fefbc80f5e74
Scotty Jacobson <ScottyMJacobson@gmail.com>
2014-11-25 01:16:54 -0500

    User registration redirects to newuser, which creates default matchingparams. Echouser and echouser/matchingparams endpoints were implemented.

--- Backend/routes.js
+++ Backend/routes.js
@@ -16,78 +16,98 @@ module.exports = function (app) {
   });
 
   app.get('/register', function(req, res) {
-      res.render('register', { });
+    res.render('register', { });
   }); 
 
   app.post('/register', function(req, res) {
     Account.register(
       new Account({ username : req.body.username }), 
       req.body.password, function(err, account) {
-        if (err) {
-            console.log("Error in Register!");
-            console.log(err);
-            return res.render('register', { info : err });
-        }
-        console.log("Registered");
-        passport.authenticate('local', 
-                              { successRedirect: '/newuser',
-                                failureRedirect: '/login' })(req,res);
-        
+      if (err) {
+        console.log("Error in Register!");
+        console.log(err);
+        return res.render('register', { info : err });
+      }
+      console.log("Registered");
+      passport.authenticate('local', 
+                  { successRedirect: '/newuser',
+                  failureRedirect: '/login' })(req,res);
+      
     });
   });
 
   app.get('/login', function(req, res) {
-      res.render('login', { user : req.user });
+    res.render('login', { user : req.user });
   });
 
   app.post('/login', passport.authenticate('local'), function(req, res) {
-      res.redirect('/');
+    res.redirect('/');
   });
 
   app.get('/newuser', function(req, res) {
-      var user = req.user;
-      if (!user) {
-        res.redirect('/?error=nologin');
-      }
-      user = Account(user);
-      user.populate();
-      console.log(user);
+    var user = req.user;
+    if (!user) {
+      res.redirect('/?error=nologin');
+    }
+
+    function returnJSON(err, jsonData) {
+      if (err) {throw err};
+      res.json(jsonData);
+    };
+    Swolemate.initMatchingParams(user, returnJSON);
+  });
+
+  app.get('/echouser', function(req, res) {
+    var user = req.user;
+    if (!user) {
+      res.redirect('/?error=nologin');
+    }
+    res.json(user);
+  });
+
+  app.get('/echouser/matchingparams', function (req, res) {
+    var user = req.user;
+    if (!user) {
+      res.redirect('/?error=nologin');
+    }
+    Account(user).populate('matching_params', function(err, mp) {
+      res.json(mp.matching_params);
+    });
   });
 
   app.post('/matchingparams', function (req, res) {
-      var user = req.user;
-      if (!user) {
-        res.redirect('/?error=nologin');
-      }
+    var user = req.user;
+    if (!user) {
+      res.redirect('/?error=nologin');
+    }
 
   });
 
   app.get('/logout', function(req, res) {
-      req.logout();
-      res.redirect('/');
+    req.logout();
+    res.redirect('/');
   });
 
   app.get('/ping', function(req, res){
-      console.log("pinging");
-      res.status(200).send("pong!");
+    console.log("pinging");
+    res.status(200).send("pong!");
   });
 
   app.get('/swolationship/:id([0-9a-f]{24})', function(req, res){
-      var idRequested = req.params['id'];
-      
+    var idRequested = req.params['id'];
+    
   });
 
   app.get('/dashboard', function(req, res) {
-      var user = req.user;
+    var user = req.user;
+    if (!user) {
+      res.redirect('/?error=nologin');
+    }
 
-      if (!user) {
-        res.redirect('/?error=nologin');
-      }
-
-      function sendEmptyDashboard () {
-        res.render('dashboard', {});
-      }
-        Swolemate.createDashboardForUser(user, sendEmptyDashboard);
+    function sendEmptyDashboard () {
+      res.render('dashboard', {});
+    }
+    Swolemate.createDashboardForUser(user, sendEmptyDashboard);
 
   });
 
--- Backend/swolemate.js
+++ Backend/swolemate.js
@@ -1,16 +1,33 @@
 var Account = require('./models/account');
 
 var Swolationship = require('./models/swolationship');
+var MatchingParams = require('./models/matchingparams');
 
 module.exports = function(app) {
   return {
 
-    createDashboardForUser: function (currentUser, callback) {
-      console.log('generating dashboard');
-      console.log(currentUser);
-      
-      callback();
-    }
+  initMatchingParams: function(currentUser, callback) {
+    var defaultMatchParams = new MatchingParams({
+      focus: "Both",
+      lat: -42,
+      lng: 31,
+      frequency: "1-2 Days"
+    });
+    defaultMatchParams.save(
+      function(err) {
+        if (err) {return console.error(err);}
+        Account.update({'username': currentUser.username}, {matching_params: defaultMatchParams}).exec(callback); 
+    });
+    
+  },
+
+  createDashboardForUser: function (currentUser, callback) {
+    console.log('generating dashboard');
+    console.log(currentUser);
+    
+    callback();
+  }
+
 
   }
 
