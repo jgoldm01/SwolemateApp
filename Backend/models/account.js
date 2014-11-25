9b4bb5b244316d3e7a27931bf0c32848985021d1
Scotty Jacobson <ScottyMJacobson@gmail.com>
2014-11-25 00:01:10 -0500

    Updated mongoose schema and models for account, swolationship, ect

--- Backend/models/account.js
+++ Backend/models/account.js
@@ -3,13 +3,18 @@ var Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
 var ObjectId = Schema.ObjectId;
 
+var MatchingParams = require('./matchingparams');
+var Swolationship = require('./swolationship');
 
 var Account = new Schema({
     username: String,
     password: String,
-    swolationship_id: ObjectId
+    paired: Boolean,
+    swolationship_id: {type: ObjectId, ref: 'Swolationship'},
+    matching_params: {type: ObjectId, ref: 'MatchingParams'} 
 });
 
 Account.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('Account', Account);
+
