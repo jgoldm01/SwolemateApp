1f05a77c0f2a7c0ea0fa3a6c0f9aab1078e66f0f
Jeremy Goldman <jeremygoldmanmd@gmail.com>
2014-12-10 01:15:40 -0500

    angularized swolematefinder

---
+++ Backend/public/js/swolefinderController.js
@@ -0,0 +1,24 @@
+var swoleMateFinder = angular.module('swoleMateFinder', []);
+
+swoleMateFinder.controller('swoleMateFinderCtrl', function ($scope) {
+  $scope.swolemates = [
+    {'name': 'Nexus S',
+     'focus': 'Fast just got faster with Nexus S.',
+ 	 'frequency': '4gb/sec',
+ 	 'distance': '5 inches'},
+    {'name': 'Nexus S',
+     'focus': 'Fast just got faster with Nexus S.',
+ 	 'frequency': '4gb/sec',
+ 	 'distance': '5 inches'},    
+ 	{'name': 'Nexus S',
+     'focus': 'Fast just got faster with Nexus S.',
+ 	 'frequency': '4gb/sec',
+ 	 'distance': '5 inches'}
+  ];
+
+  // todo: $http.get(thingy.json).success(function(data) {
+  // 	$scope.swolemates = data;
+  // })
+
+  $scope.ppl = {'name': 'johnny boy'}
+});
