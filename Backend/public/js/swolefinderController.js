340a39b14f9c3dc9b2b7e2c1c58b17935d95a102
Jeremy Goldman <jeremygoldmanmd@gmail.com>
2014-12-10 03:41:56 -0500

    swolefinder edited

--- Backend/public/js/swolefinderController.js
+++ Backend/public/js/swolefinderController.js
@@ -1,27 +1,27 @@
 var swoleMateFinder = angular.module('swoleMateFinder', []);
 
 swoleMateFinder.controller('swoleMateFinderCtrl', function ($scope, $http) {
-  $scope.swolemates = [
-    {'name': 'Nexus S',
-     'focus': 'Fast just got faster with Nexus S.',
- 	 'frequency': '4gb/sec',
- 	 'distance': '5 inches'},
-    {'name': 'Nexus S',
-     'focus': 'Fast just got faster with Nexus S.',
- 	 'frequency': '4gb/sec',
- 	 'distance': '5 inches'},    
- 	{'name': 'Nexus S',
-     'focus': 'Fast just got faster with Nexus S.',
- 	 'frequency': '4gb/sec',
- 	 'distance': '5 inches'}
-  ];
+  // $scope.swolemates = [
+  //   {'name': 'Nexus S',
+  //    'focus': 'Fast just got faster with Nexus S.',
+ 	//  'frequency': '4gb/sec',
+ 	//  'distance': '5 inches'},
+  //   {'name': 'Nexus S',
+  //    'focus': 'Fast just got faster with Nexus S.',
+	 // 	 'frequency': '4gb/sec',
+	 // 	 'distance': '5 inches'},    
+	 // 	{'name': 'Nexus S',
+	 //   'focus': 'Fast just got faster with Nexus S.',
+	 // 	 'frequency': '4gb/sec',
+	 // 	 'distance': '5 inches'}
+	 //  ];
 
   $scope.selectswolemate = function(name) {
   	$http.post('api/chooseswolemate', {"username": name})
   	window.location.replace("dashboard.html");
   }
 	
-  // todo: $http.get(api/swolefinder).success(function(data) {
-  // 	$scope.swolemates = data;
-  // })
+  $http.get('api/swolefinder').success(function(data) {
+   	$scope.swolemates = data;
+  })
 });
