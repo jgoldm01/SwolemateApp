17a3c3fcf8c1af1c1a97d58b167359f5797b4406
Scotty Jacobson <ScottyMJacobson@gmail.com>
2014-12-10 06:03:23 -0500

    Jeremy fucked up swolefinderController so I fixed it by moving success to where it was supposed to be =P

--- Backend/public/js/swolefinderController.js
+++ Backend/public/js/swolefinderController.js
@@ -4,27 +4,26 @@ swoleMateFinder.controller('swoleMateFinderCtrl', function ($scope, $http) {
   // $scope.swolemates = [
   //   {'name': 'Nexus S',
   //    'focus': 'Fast just got faster with Nexus S.',
- 	//  'frequency': '4gb/sec',
- 	//  'distance': '5 inches'},
+  //  'frequency': '4gb/sec',
+  //  'distance': '5 inches'},
   //   {'name': 'Nexus S',
   //    'focus': 'Fast just got faster with Nexus S.',
-	 // 	 'frequency': '4gb/sec',
-	 // 	 'distance': '5 inches'},    
-	 // 	{'name': 'Nexus S',
-	 //   'focus': 'Fast just got faster with Nexus S.',
-	 // 	 'frequency': '4gb/sec',
-	 // 	 'distance': '5 inches'}
-	 //  ];
+   //    'frequency': '4gb/sec',
+   //    'distance': '5 inches'},    
+   //   {'name': 'Nexus S',
+   //   'focus': 'Fast just got faster with Nexus S.',
+   //    'frequency': '4gb/sec',
+   //    'distance': '5 inches'}
+   //  ];
   $http.get('api/swolefinder').success(function(data) {
     $scope.swolemates = data;
   })
 
   $scope.selectswolemate = function(username) {
-  	$http.post('api/chooseswolemate', {"username": username})
-  	
-  }.success(function(data, status, headers, config) {
-    window.location.replace("dashboard.html");
-  })
-	
+    $http.post('api/chooseswolemate', {"username": username})
+    .success(function(data, status, headers, config) {
+    window.location.href = "dashboard";
+    })
+  }
 
 });
