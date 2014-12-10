7469eff17679a03a7875d9c525fecc4ae70137ed
npham02 <Nga.Pham@tufts.edu>
2014-12-10 03:23:53 -0500

    Added not working button and current goals

--- Backend/public/js/dashboard.js
+++ Backend/public/js/dashboard.js
@@ -13,6 +13,18 @@ dashBoard.controller('dashBoardCtrl', function ($scope) {
    'complete': 'true/sec'}
   ];
 
+  $scope.currentGoals = [
+    {'name': 'currentGoals',
+     'body': 'Fast just got faster with Nexus S.',
+   'complete': 'true/sec'},
+   {'name': 'currentGoals S',
+     'body': 'Fast just got faster with Nexus S.',
+   'complete': 'true/sec'},
+   {'name': 'currentGoals S',
+     'body': 'Fast just got faster with Nexus S.',
+   'complete': 'true/sec'}
+  ];
+
   // todo: $http.get(api/swolefinder).success(function(data) {
   //  $scope.swolemates = data;
   // })
